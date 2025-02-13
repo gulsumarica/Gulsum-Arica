9a5fa20f647baf8cf935b1bf9feb4e4ae3310448
Gülsüm Arıca <gulsumarica.biz@gmail.com>
2025-02-13 18:40:46 +0300

    dil için tasarım değişikliği yapıldı

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,39 +1,34 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setLanguage } from "../redux/actions/actions";
+import Flag from "react-world-flags";
 
 const LanguageSwitcher = () => {
   const dispatch = useDispatch();
-  const currentLanguage = useSelector((state) => state.language);
+  const currentLanguage = useSelector(
+    (state) => state.language.selectedLanguage
+  );
+
+  const toggleLanguage = () => {
+    dispatch(setLanguage(currentLanguage === "tr" ? "en" : "tr"));
+  };
 
   return (
     <div>
       <div className="flex flex-row gap-2 items-center">
-        {/* Türkçe dilini seçme */}
         <a
           href="#"
           onClick={(e) => {
-            e.preventDefault(); // Sayfa yeniden yüklenmesin
-            dispatch(setLanguage("tr"));
+            e.preventDefault();
+            toggleLanguage();
           }}
-          className={`text-[#3730A3] ${
-            currentLanguage === "tr" ? "font-bold" : ""
-          }`}
+          className={`text-[#3730A3] font-bold`}
         >
-          Türkçe
-        </a>
-        {/* İngilizce dilini seçme */}
-        <a
-          href="#"
-          onClick={(e) => {
-            e.preventDefault(); // Sayfa yeniden yüklenmesin
-            dispatch(setLanguage("en"));
-          }}
-          className={`text-[#3730A3] ${
-            currentLanguage === "en" ? "font-bold" : ""
-          }`}
-        >
-          English
+          {currentLanguage === "tr" ? (
+            <Flag code="GB" className="w-8 h-full rounded-sm" />
+          ) : (
+            <Flag code="TR" className="w-8 h-full rounded-sm" />
+          )}
         </a>
       </div>
     </div>
